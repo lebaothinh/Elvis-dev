b6e693fb8a2fd8a0cfc5a65a59caa23111b4d73e
admin <user1@user1noMacBook-Pro.local>
2023-09-23 14:52:35 +0700

    Update CI/CD

--- src/app/component/HeaderMenu.tsx
+++ src/app/component/HeaderMenu.tsx
@@ -14,31 +14,28 @@ export const HeaderMenu = () => {
       className="w-full h-full text-center flex flex-col mt-4 text-outer-space uppercase text-sm font-medium lg:flex-row lg:space-x-4 lg:mt-0"
     >
       <li className="w-full text-center">
-        <Link
-          scroll={false}
+        <a
           href="/"
           className="w-[150px] my-2 mx-auto h-[27px] lg:w-[118px] rounded-full hover:border-[1px] transition-all before:block before:bottom-[-2px] before:w-[80%] before:h-[2px]  before:absolute before:left-[50%] before:translate-x-[-50%] before:bg-cultured  after:block after:top-[-2px] after:w-[80%] after:h-[2px] relative after:absolute after:left-[50%] after:translate-x-[-50%] after:bg-cultured  hover:text-myrtle-green hover:border-myrtle-green flex justify-center items-center"
         >
           Home
-        </Link>
+        </a>
       </li>
       <li className="w-full text-center">
-        <Link
-          scroll={false}
+        <a
           href="/#work-section"
           className="w-[150px] my-2 mx-auto h-[27px] lg:w-[118px] rounded-full hover:border-[1px] transition-all before:block before:bottom-[-2px] before:w-[80%] before:h-[2px]  before:absolute before:left-[50%] before:translate-x-[-50%] before:bg-cultured  after:block after:top-[-2px] after:w-[80%] after:h-[2px] relative after:absolute after:left-[50%] after:translate-x-[-50%] after:bg-cultured  hover:text-myrtle-green hover:border-myrtle-green flex justify-center items-center"
         >
           Works
-        </Link>
+        </a>
       </li>
       <li className="w-full text-center">
-        <Link
-          scroll={false}
+        <a
           href="/#experience-section"
           className="w-[150px] my-2 mx-auto h-[27px] lg:w-[118px] rounded-full hover:border-[1px] transition-all before:block before:bottom-[-2px] before:w-[80%] before:h-[2px]  before:absolute before:left-[50%] before:translate-x-[-50%] before:bg-cultured  after:block after:top-[-2px] after:w-[80%] after:h-[2px] relative after:absolute after:left-[50%] after:translate-x-[-50%] after:bg-cultured  hover:text-myrtle-green hover:border-myrtle-green flex justify-center items-center"
         >
           Experience
-        </Link>
+        </a>
       </li>
       <li className="w-full text-center">
         <Link
